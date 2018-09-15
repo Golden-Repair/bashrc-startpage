c6671245664df39d7e15d0bc8702d2129340aab0
Claudio Brasser <brasser13@gmail.com>
2018-09-15 18:31:44 +0200

    complete rework of file system and some clean up, all commands should be functional now

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Start-Page
 Start page made after a linux terminal with functional commands for shortcuts and searches.
 
+Directories are supposed to serve as bookmark categories and 'files' as bookmarks/links.
+
+for navigation commands both absolute and relative paths are supported, only paths like 
+"cd ../dir/" are not supported atm.
+
+Same goes for mkdir, rmdir, touch, and rm. 
+
+You can open shortcuts and enter folders also by selecting them with the tab key and hitting enter or mouse clicking
+
+the "ls" command can list contents of an absolute path or a relative path in the subdirectory of the current location
+
 Commands supported:
 Touch [name] [url] (create link in current dir)
 
@@ -16,15 +27,18 @@ ls
 
 clear
 
-cd
+cd | cd .. | cd [path]
 
 locate [query](duckduckgo search)
 
+Links and directories are saved to the local storage now, if there is no config found 
+in local storage the page will just create an empty home directory
+
 # Planned additions
 
-I want to export folders and links into a config file so that changes can be made permanent. I figured this would be the most appropriate way of saving stuff since I don't really plan on hosting it anywhere.
+I plan on giving some color options for links although I like having files/directories on a clearly distinct color.
 
-Also i plan on giving some color options for links although I like having files/directories on a clearly distinct color.
+In the future i will add the possibilities to type the name of a file/link that is stored as a command to open the link stored in that file
 
 ## Planned commands
 notes - quick way of adding notes/todos and removing them. Not sure how I plan on displaying it for now.
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
-
 const separator = '/'
+const dirNameExp =  /[\w|\~]+$/
+const containsSlashCheckExp = /\//
+const isAbsolutePathExp = /^\~/
 
 const commands = [
 	Command("locate","search anythin on the internet"),
@@ -15,9 +17,15 @@ const commands = [
 	Command("echo", "repeat something")
 ]
 
+var allDirectories = []
+var allFiles = []
 
-var curr_path = []
+var curr_dir
 
+
+/*
+*Parse user input query from command line and execute command
+*/
 function parseQuery() {
 	let query = document.getElementById('input_field').value
 	let command = query.split(' ')[0]
@@ -33,16 +41,25 @@ function parseQuery() {
 		clearConsoleOut()
 		switch(command){
 			case "pwd":
-				printMessage(buildPathString())
+				printMessage(curr_dir.getPath())
 				break
 			case "ls":
-				let dirs = curr_path[curr_path.length-1].getChildrenNames()
-				let links = curr_path[curr_path.length-1].getLinkNames()
-				for(let k =0;k<dirs.length;k++){
-					printDirectory(dirs[k])
-				}
-				for(let k =0;k<links.length;k++){
-					printLink(links[k])
+				if(args[0] == undefined){
+					let dirs = curr_dir.getSubdirNames()
+					let links = curr_dir.getLinkNames()
+					for (let x of dirs) printDirectory(x)
+					for (let x of links) printLink(x)
+				} else {
+					let tempDir
+					if(isAbsolutePathExp.test(args[0])){
+						tempDir = getDirWithPath(args[0])
+					} else {
+						tempDir = getDirWithPath(curr_dir.getPath()+separator+args[0])
+					}
+					let dirs = tempDir.getSubdirNames()
+					let links = tempDir.getLinkNames()
+					for (let x of dirs) printDirectory(x)
+					for (let x of links) printLink(x)
 				}
 				break
 			case "cd":
@@ -54,7 +71,9 @@ function parseQuery() {
 						homeDir()
 						break
 					default:
-						enterDir(args[0])
+						if(!enterDir(args[0])){
+							printMessage("no such file or directory: "+args[0])
+						}
 						break
 				}
 				break
@@ -67,22 +86,29 @@ function parseQuery() {
 				
 				break
 			case "mkdir":
-				if(!curr_path[curr_path.length-1].addDirectoryByName(args[0])){
-					printMessage("failed to create '"+args[0]+"': Directory already exists")
+				if(!mkdir(args[0])){
+					printMessage("cannot create directory '"+args[0]+"': File exists")
 				}
+	
 				break
 			case "rmdir":
-				if (!curr_path[curr_path.length-1].removeDirectory(args[0])){
+				let result = rmdir(args[0])
+				//Dir does not exist
+				if(result == 0){
 					printMessage("failed to remove '"+args[0]+"': No such file or directory")
+				//Dir is not empty
+				}else if (result == 1){
+					printMessage("failed to remove '"+args[0]+"': Directory not empty")
 				}
+				//Otherwise dir is deleted
 				break
 			case "touch":
-				if(!curr_path[curr_path.length-1].addLink(args[0], args[1])){
-					printMessage("failed to create '"+args[0]+"': Link already exists")
+				if(!touch(args[0], args[1])){
+					printMessage("cannot create file '"+args[0]+"': File exists")	
 				}
 				break
 			case "rm":
-				if (!curr_path[curr_path.length-1].removeLink(args[0])){
+				if(!rm(args[0])){
 					printMessage("failed to remove '"+args[0]+"': No such file or directory")
 				}
 				break
@@ -91,10 +117,13 @@ function parseQuery() {
 				break
 			case "echo":
 				printMessage(args.join(' '))
+				break
 		}
 		document.getElementById('input_field').value = ''
 	}
 }
+
+//------------------------------console stuff-----------------------------
 
 function printDirectory(name){
 	let a = document.createElement('a')
@@ -112,12 +141,12 @@ function printMessage(msg){
 	a.appendChild(linkText)
 	document.getElementById('console_out').appendChild(a)
 }
-function printLink(name){
+function printLink(path){
 	let a = document.createElement('a')
 	a.className = "cyan"
 	a.target ="blank"
-	a.href=curr_path[curr_path.length-1].getUrl(name)
-	let linkText = document.createTextNode(name)
+	a.href=getFileWithPath(path).getUrl()
+	let linkText = document.createTextNode(getFileWithPath(path).getName())
 	a.appendChild(linkText)
 	document.getElementById('console_out').appendChild(a)
 }
@@ -129,188 +158,229 @@ function clearConsoleOut(){
 	}
 }
 
-function init() {
-	updatewd()
-	var home= Directory("~")
-	var reddit= Directory("reddit")
-	var university= Directory("university")
-	var general = Directory("general")
-	reddit.addLink("r/startpages","https://reddit.com/r/startpages")
-	reddit.addLink("r/unixporn", "https://www.reddit.com/r/unixporn/")
-	general.addLink("youtube","https://www.youtube.com/")
-	general.addLink("github","https://github.com/")
-	general.addLink("stackowerflow","https://stackoverflow.com/")
-	home.addDirectoryByReference(reddit)
-	home.addDirectoryByReference(general)
-	home.addDirectoryByReference(university)
-	curr_path.push(home)
-
-}
-function buildPathString (){
-	let path =""
-	for(let i =0;i<curr_path.length;i++){
-		path += curr_path[i].getName()+'/'
-	}
-	return path
-}
-
 function updatewd(){
-	document.getElementById('location').textContent = buildPathString()
+	document.getElementById('location').textContent = curr_dir.getPath()
+}
+
+function init() {
+	loadConfigFromLocalStorage()
+	for (let d of allDirectories){
+		if(d.getPath() == '~'){
+			curr_dir = d
+		}
+	}
+	if (curr_dir == undefined ){
+		curr_dir = Directory('~')
+		allDirectories.push(curr_dir)
+	}
+	updatewd()
+	
 }
 
 
-function enterDir(name){
-	if(curr_path[curr_path.length-1].containsDir(name)){
-		curr_path.push(curr_path[curr_path.length-1].getDirectory(name))
-		updatewd()
-		clearConsoleOut()
-		document.getElementById("input_field").focus()
-	} else{
-		printMessage("No such directory: "+name)
+
+//-------------------------------------File system operations-----------------------------
+
+
+function mkdir(path){
+	//check if submitted path is absolute
+	if(isAbsolutePathExp.test(path)){
+		//Check if path is not already taken
+		if(dirExists(path)){
+			return false
+		}
+		//check if path is viable
+		if(pathIsAvailable(path)){
+			allDirectories.push(Directory(path))
+			storeConfigToLocalStorage()
+			return true	
+		}
+		
+	} else {
+		let newPath = curr_dir.getPath() + separator + path
+		return mkdir(newPath)
+	}
+}
+
+function rmdir(path){
+	if(isAbsolutePathExp.test(path)){
+		//Check if dir exists and is not empy
+		if(!dirExists(path)){
+			return 0
+		}
+		if(!getDirWithPath(path).isEmpty){
+			return 1
+		}
+		for(let i =0;i<allDirectories.length;i++){
+			if(allDirectories[i].getPath() == path){
+				allDirectories.splice(i,1)
+				storeConfigToLocalStorage()
+				return 2
+			}
+		}
+	} else {
+		//build absolute path and make recursive call
+		let newPath = curr_dir.getPath() + separator + path
+		return rmdir(newPath)
+	}
+}
+
+function touch(path, url){
+	//check if submitted path is absolute
+	if(isAbsolutePathExp.test(path)){
+		//Check if path is not already taken
+		if(fileExists(path)){
+			return false
+		}
+		//check if path is viable
+		if(pathIsAvailable(path)){
+			allFiles.push(Link(path, url))
+			storeConfigToLocalStorage()
+			return true	
+		}
+		
+	} else {
+		//build absolute path and make recursive call
+		let newPath = curr_dir.getPath() + separator + path
+		return touch(newPath, url)
+	}
+}
+
+function rm(path){
+	if(isAbsolutePathExp.test(path)){
+		//Check if file exists
+		if(!fileExists(path)){
+			return false
+		}
+		for(let i =0;i<allFiles.length;i++){
+			if(allFiles[i].getPath() == path){
+				allFiles.splice(i,1)
+				storeConfigToLocalStorage()
+				return true
+			}
+		}
+	} else {
+		let newPath = curr_dir.getPath() + separator + path
+		return rm(newPath)
+	}
+}
+
+
+//-------------------------------------Navigation------------------------------------
+
+function enterDir(path){
+	if(isAbsolutePathExp.test(path)){
+		if(dirExists(path)){
+			for (let dir of allDirectories){
+				if(dir.getPath() == path){
+					curr_dir = dir
+					updatewd()
+					clearConsoleOut()
+					return true
+				}
+			}
+		} else {
+			return false
+		}
+		
+	} else {
+		let newPath = curr_dir.getPath() + separator + path
+		if(dirExists(newPath)){
+			for (let dir of allDirectories){
+				if(dir.getPath() == newPath){
+					curr_dir = dir
+					updatewd()
+					clearConsoleOut()
+					return true
+				}
+			}
+		} else {
+			return false
+		}
 	}
 
 }
 
 function homeDir(){
-	curr_path = curr_path.slice(0,1)
-	updatewd()
+	for (let dir of allDirectories){
+		if(dir.getPath() == '~'){
+			curr_dir = dir
+			updatewd()
+			clearConsoleOut()		
+		}
+	}
 }
 
 function upDir(){
-	//Already in home directory, no root access :P
-	if(curr_path.length == 1){
-		return
-	} else {
-		curr_path.pop()
-		updatewd()
+	//Make sure we are not on the home directory
+	if(!(curr_dir.getPath() == '~')){
+		let newPath = curr_dir.getPath().split('/')
+		newPath.pop()
+		newPath = newPath.join('/')
+		enterDir(newPath)
 	}
 }
 
-function checkIfDirValid(){
+//-------------------------------------Classes------------------------------------
 
-}
 
-function Directory (name) {
+function Directory (path) {
 	var self = {}
-	self.name = name
-	self.children = []
-	self.links = []
+	self.path = path
 
 	self.getName = function (){
-		return this.name
+		return this.path.match(dirNameExp)[0]
 	}
-	self.getChildren = function (){
-		return this.children
+	self.getPath = function (){
+		return this.path
 	}
-	self.getChildrenNames = function (){
+	self.getSubdirNames = function (){
 		let names = []
-		for(let i =0;i<self.children.length;i++){
-			names.push(self.children[i].getName())
+		for (let dir of allDirectories){
+			let myPath = this.path.split('/')
+			let subPath = dir.getPath().split('/')
+			subPath.pop()
+
+			if(myPath.join('/') == subPath.join('/')){
+				names.push(dir.getName())
+			}
 		}
 		return names
+	}
+
+	self.isEmpty = function (){
+		return this.getSubdirNames().length == 0 && this.getLinkNames().length == 0
 	}
 	self.getLinkNames = function (){
 		let names = []
-		for(let i =0;i<self.links.length;i++){
-			names.push(self.links[i].getName())
+		for (let file of allFiles){
+			let myPath = this.path.split('/')
+			let subPath = file.getPath().split('/')
+			subPath.pop()
+
+			if(myPath.join('/') == subPath.join('/')){
+				names.push(file.getPath())
+			}
 		}
 		return names
 	}
-	self.getFiles = function (){
-		return this.files
-	}
-	self.addDirectoryByName = function(name){
-		if(!self.containsDir()){
-			self.children.push(Directory(name))
-			return true
-		}else {
-			return false
-		}
-		
-	}
-	self.addDirectoryByReference = function(dir){
-		if(!self.containsDir()){
-			self.children.push(dir)
-			return true
-		}else {
-			return false
-		}
-	}
-	self.addLink = function(name,url){
-		if(!self.containsLink()){
-			self.links.push(Link(name,url))
-			return true
-		}else {
-			return false
-		}
-		
-	}
-	self.containsLink = function(name){
-		for(let i =0;i<self.links.length;i++){
-			if(self.links[i].getName() == name){
-				return true
-			}
-		}
-		return false
-	}
-	self.getUrl = function(name){
-		for(let i =0;i<self.links.length;i++){
-			if(self.links[i].getName() == name){
-				return self.links[i].getUrl()
-			}
-		}
-	}
-	self.containsDir = function(dir){
-		for(let i =0;i<self.children.length;i++){
-			if(self.children[i].getName() == dir){
-				return true
-			}
-		}
-
-		return false
-	}
-	self.getDirectory = function(dir){
-		for(let i =0;i<self.children.length;i++){
-			if(self.children[i].getName() == dir){
-				return self.children[i]
-			}
-		}
-		return null
-	}
-	self.removeDirectory = function(dir){
-		for(let i =0;i<self.children.length;i++){
-			if(self.children[i].getName() == dir){
-				self.children.splice(i,1)
-				return true
-			}
-		}
-		return false
-	}
-
-	self.removeLink = function(link){
-		for(let i =0;i<self.links.length;i++){
-			if(self.links[i].getName() == link){
-				self.links.splice(i,1)
-				return true
-			}
-		}
-		return false
-	}
-
 	return self
 }
 
-function Link (name, url) {
+function Link (path, url) {
 	var self = {}
-	self.name = name
 	self.url = url
+	self.path = path
 
 	self.getUrl = function(){
 		return this.url
 	}
+	self.getPath = function(){
+		return this.path
+	}
 
 	self.getName = function(){
-		return this.name
+		return this.path.match(dirNameExp)[0]
 	}
 	return self
 }
@@ -329,4 +399,82 @@ function Command (name, description) {
 	}
 
 	return self
+}
+
+//-------------------------------------Helper functions------------------------------------
+
+
+function pathIsAvailable(path){
+	for(let i = 0;i<allDirectories.length;i++){	
+			let temp = path.split('/')
+			temp.pop()
+			temp = temp.join('/')
+			if(temp == allDirectories[i].getPath()){
+				//Path is viable
+				return true
+			}
+		}
+		//Did not find a parent directory that matches the path until the name
+		return false
+}
+
+/*
+Check if an Absolute path is already in the file system
+*/
+function dirExists(path){
+	for (let dir of allDirectories){
+		if(dir.getPath() == path){
+			return true
+		}
+	}
+	return false
+}
+
+function fileExists(path){
+	for (let file of allFiles){
+		if(file.getPath() == path){
+			return true
+		}
+	}
+	return false
+}
+
+function getDirWithPath(path){
+	for (let dir of allDirectories){
+		if(dir.getPath() == path){
+			return dir
+		}
+	}
+	return null
+}
+
+function getFileWithPath(path){
+	for (let file of allFiles){
+		if(file.getPath() == path){
+			return file
+		}
+	}
+	return null
+}
+
+//--------------------------------------Saving & Restoring file system------------------------
+
+function loadConfigFromLocalStorage(){
+	let dirs = JSON.parse(window.localStorage.getItem("directories"))
+	if(!dirs){
+		allDirectories.push(Directory('~'))
+		return
+	}
+	for (let d of dirs){
+		allDirectories.push(Directory(d.path))
+	}
+	let files = JSON.parse(window.localStorage.getItem("files"))
+	for (let f of files){
+		allFiles.push(Link(f.path,f.url))
+	}
+}
+
+function storeConfigToLocalStorage(){
+	window.localStorage.setItem("directories", JSON.stringify(allDirectories))
+	window.localStorage.setItem("files", JSON.stringify(allFiles))
 }

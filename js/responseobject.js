1eca672dda0a22dc1ec2b7d110ed1e5149b135fe
cbrasser <brasser13@gmail.com>
2019-12-11 19:22:07 +0100

    cleaned up

--- js/responseobject.js
+++
@@ -1,6 +0,0 @@
-class ResponseObject {
-    constructor(resultType, messages) {
-        this.resultType = resultType;
-        this.messages = typeof messages === 'string' ? this.messages = [messages] : this.messages = messages;
-    }
-}
